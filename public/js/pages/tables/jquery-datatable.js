8f6b9811d9ae1e22ac8008ad240f54c437422bd4
Anjeyster <anjeyster@gmail.com>
2019-07-01 13:27:36 +0530

    Updated

--- public/js/pages/tables/jquery-datatable.js
+++ public/js/pages/tables/jquery-datatable.js
@@ -3,12 +3,16 @@ function formatDate() {
     var d = new Date(),
         month = '' + (d.getMonth() + 1),
         day = '' + d.getDate(),
-        year = d.getFullYear();
+        year = d.getFullYear(),
+        hour = d.getHours(),
+        min = d.getMinutes(),
+        sec = d.setSeconds()
+    ;
 
     if (month.length < 2) month = '0' + month;
     if (day.length < 2) day = '0' + day;
 
-    return [year, month, day].join('-');
+    return [year, month, day].join('-')+" "+[hour, min, sec].join("-");
 }
 
 $(function () {
